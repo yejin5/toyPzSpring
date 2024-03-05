ae1fe12d109be0caf4f1e012ec70b62945bc99e4
yejin5 <ohyejin1117@gmail.com>
2024-03-05 16:07:20 +0900

    H2 DB connect by JDBC

--- src/main/java/toypz/hellospring/SpringConfig.java
+++ src/main/java/toypz/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package toypz.hellospring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import toypz.hellospring.repository.JdbcMemberRepository;
 import toypz.hellospring.repository.MemberRepository;
 import toypz.hellospring.repository.MemoryMemberRepository;
 import toypz.hellospring.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +27,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
